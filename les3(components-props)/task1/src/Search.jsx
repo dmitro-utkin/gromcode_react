930dd1427f836a05f82ae7717a40950647f20934
ДМИТРО <sssddd1366628@gmail.com>
2024-07-01 20:36:30 +0300

    + export

--- les3(components-props)/task1/src/Search.jsx
+++ les3(components-props)/task1/src/Search.jsx
@@ -5,7 +5,7 @@ import "./search.scss";
 
 const rootElement = document.querySelector("#root");
 
-const Search = (props) => {
+export const Search = (props) => {
   const element = (
     <div className="search">
       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
