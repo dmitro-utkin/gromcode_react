fce8dc485c810210d67c5de473fb273592d7abc2
ДМИТРО <sssddd1366628@gmail.com>
2024-07-03 10:54:54 +0300

    del point

--- les3(components-props)/task4(greeting-component)/src/Greeting.jsx
+++ les3(components-props)/task4(greeting-component)/src/Greeting.jsx
@@ -15,7 +15,7 @@ function Greeting({ firstName, lastName, birthDate }) {
 
   return (
     <div className="greeting">
-      My name is {firstName} {lastName}. I am {age} years old.
+      My name is {firstName} {lastName}. I am {age} years old
     </div>
   );
 }
