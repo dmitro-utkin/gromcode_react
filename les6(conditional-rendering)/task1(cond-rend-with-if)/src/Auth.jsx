17bd18db23b88b9d1b9dbfffe97a7f928e5a33c2
ДМИТРО <sssddd1366628@gmail.com>
2024-07-05 11:52:17 +0300

    try todo les6/task1

---
+++ les6(conditional-rendering)/task1(cond-rend-with-if)/src/Auth.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import Greeting from "./Greeting.jsx";
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  handleLogin = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+
+  render() {
+    // const button = this.state.isLoggedIn
+    // ?<button onClick={this.handleLogout}>Logout</button>
+    // : <button onClick={this.handleLogin}>Login</button>;
+
+    // let button;
+
+    // if (this.state.isLoggedIn) {
+    //   button = <button onClick={this.handleLogout}>Logout</button>;
+    // } else {
+    //   button = <button onClick={this.handleLogin}>Login</button>;
+    // }
+
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+
+        {this.state.isLoggedIn ? (
+          <button className="btn logout" onClick={this.handleLogout}>Logout</button>
+        ) : (
+          <button className="btn login" onClick={this.handleLogin}>Login</button>
+        )}
+
+        {/* <div>
+          {button}
+          {/* <button onClick={this.handleLogin}>Login</button>
+          <button onClick={this.handleLogout}>Logout</button> */}
+        {/* </div> */}
+      </div>
+    );
+  }
+}
+
+export default Auth;
