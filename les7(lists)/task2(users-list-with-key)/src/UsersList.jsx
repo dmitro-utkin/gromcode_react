45ce48dc73eb9b2f5cefe92d3c238c19c8231a93
ДМИТРО <sssddd1366628@gmail.com>
2024-07-07 16:08:34 +0300

    try again

--- les7(lists)/task2(users-list-with-key)/src/UsersList.jsx
+++ les7(lists)/task2(users-list-with-key)/src/UsersList.jsx
@@ -7,33 +7,34 @@ class UsersList extends React.Component {
   };
 
   toggleSorting = () => {
-    const newSorting = this.state.sorting === "desc" 
-      ? "asc" 
-      : "desc";
+    const newSorting = this.state.sorting === "asc" 
+      ? "desc" 
+      : "asc";
     this.setState({
       sorting: newSorting,
     });
   };
 
   render() {
-    console.log(this.props.users);
-    let usersList = [...this.props.users];
-
+    let usersList;
+  
     if (this.state.sorting) {
-      usersList.sort((a, b) => 
+      usersList = this.props.users.slice().sort((a, b) => 
         this.state.sorting === "asc"
           ? a.age - b.age
           : b.age - a.age
-      )
+      );
+    } else {
+      usersList = this.props.users;
     }
-
+  
     return (
       <div>
         <button className="btn" onClick={this.toggleSorting}>
           {this.state.sorting || '-'}
         </button>
         <ul className="users">
-          {this.props.users.map((user) => (
+          {usersList.map(user => (
             <User key={user.id} {...user} />
           ))}
         </ul>
