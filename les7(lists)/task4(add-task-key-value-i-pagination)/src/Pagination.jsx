f6cbfac45af0b8726fafbf6365369936dcb5efcf
ДМИТРО <sssddd1366628@gmail.com>
2024-07-07 17:51:24 +0300

    try to fix bugs

--- les7(lists)/task4(add-task-key-value-i-pagination)/src/Pagination.jsx
+++ les7(lists)/task4(add-task-key-value-i-pagination)/src/Pagination.jsx
@@ -7,11 +7,11 @@ const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) =
 
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
+      <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable || currentPage === 1}>
         ←
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
+      <button className="btn" onClick={goNext} disabled={!isNextPageAvailable || totalItems % itemsPerPage === 0}>
         →
       </button>
     </div>
