b2d8dcbd8f92b4911bd6acce513afaa1c6e4c14a
ДМИТРО <sssddd1366628@gmail.com>
2024-07-07 13:57:19 +0300

    fixed name of folder

--- les7(lists)/task1(rendering-list)/src/index.jsx
+++ les7(lists)/task1(rendering-list)/src/index.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import NumberList from "./NumberList";
+import NumbersList from "./NumbersList";
 
 const rootElement = document.querySelector("#root");
 
 const numbers = [1, 2, 3, 4, 5];
 
-ReactDOM.render(<NumberList numbers={numbers} />, rootElement);
-
-
-
-
+ReactDOM.render(<NumbersList numbers={numbers} />, rootElement);
 
 // const numberElements = numbers.map((num) => <li>{num}</li>);
 
