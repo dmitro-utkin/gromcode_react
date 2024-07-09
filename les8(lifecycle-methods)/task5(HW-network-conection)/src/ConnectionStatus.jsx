ec81ed8f455f41a1d3e920170072f8d0907304e5
ДМИТРО <sssddd1366628@gmail.com>
2024-07-09 20:12:11 +0300

    try todo task5(HW)

---
+++ les8(lifecycle-methods)/task5(HW-network-conection)/src/ConnectionStatus.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class ConnectionStatus extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOnline: true
+    };
+  }
+
+  handleStatusChange = () => {
+    this.setState({ isOnline: navigator.onLine });
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', this.handleStatusChange);
+    window.addEventListener('offline', this.handleStatusChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.handleStatusChange);
+    window.removeEventListener('offline', this.handleStatusChange);
+  }
+
+  render() {
+    const statusClass = this.state.isOnline ? '' : 'status_offline';
+    const statusText = this.state.isOnline ? 'Online' : 'Offline';
+    return <div className={`status ${statusClass}`}>{statusText}</div>;
+  }
+}
+
+export default ConnectionStatus;
