38f95ae89e820d1132f384e32998452ede01184b
ДМИТРО <sssddd1366628@gmail.com>
2024-07-17 08:23:19 +0300

    try todo les10/task2

--- les10(lifting-state-up)/task2(products-list)/src/ShoppingCart.jsx
+++ les10(lifting-state-up)/task2(products-list)/src/ShoppingCart.jsx
@@ -7,6 +7,11 @@ class ShoppingCart extends Component {
     cartItems: [
       { id: 1, name: "iPhone 11", price: 999 },
       { id: 2, name: "iPad Pro", price: 799 },
+      { id: 3, name: "MacBook Pro", price: 1299 },
+      { id: 4, name: "Apple Watch", price: 399 },
+      { id: 5, name: "Apple TV", price: 199 },
+      { id: 6, name: "AirPods", price: 249 },
+      { id: 7, name: "Mac Studio", price: 1999 },      
     ],
   };
 
