56a5c63fef0d3a536bdb9756b4cc58deb79f208e
ДМИТРО <sssddd1366628@gmail.com>
2024-07-21 14:35:21 +0300

    try todo les11/ task2

---
+++ les11(component-updates)/task2(children)/src/Dialog.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Dialog = ({ isOpen, children, title, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div class="dialog">
+      <div class="dialog__heading">
+        <h4 class="dialog__title">{title}</h4>
+        <button class="dialog__close-btn" onClick={onClose}>
+          +
+        </button>
+      </div>
+      <div class="dialog__content">
+        <p>{children}</p>
+      </div>
+    </div>
+  );
+};
+
+Dialog.propTypes = {
+  isOpen: PropTypes.bool,
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+};
+
+Dialog.defaultProps = {
+  title: "",
+  isOpen: false,
+}
+
+export default Dialog;
