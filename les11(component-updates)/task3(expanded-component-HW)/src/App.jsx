ea757e6f665c20c9581bd32004cd40e6b197ce2c
ДМИТРО <sssddd1366628@gmail.com>
2024-07-24 15:09:48 +0300

    +3

--- les11(component-updates)/task3(expanded-component-HW)/src/App.jsx
+++ les11(component-updates)/task3(expanded-component-HW)/src/App.jsx
@@ -3,16 +3,15 @@ import Expand from "./Expand.jsx";
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
-
   return (
     <div className="app">
       <Expand isOpen={isOpen} title="Some title" handleToggle={handleToggle}>
         <p>
-          Hooks are a new addition in React 16.8. They let you use state and other React features without writing a class.
+          Hooks are a new addition in React 16.8. They let you use state and
+          other React features without writing a class.
         </p>
       </Expand>
     </div>
--- les11(component-updates)/task3(expanded-component-HW)/src/Expand.jsx
+++ les11(component-updates)/task3(expanded-component-HW)/src/Expand.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Expand = ({ isOpen, children, title, handleToggle }) => {
-  const toggleIcon = isOpen ? faChevronUp : faChevronDown;
+  const toggleIcon = isOpen ? "fa-chevron-up" : "fa-chevron-down";
 
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="btn" onClick={handleToggle}>
-        <i
-              className={`expand__toggle-icon fa ${isOpen ? "fa-chevron-up" : "fa-chevron-down"}`}
-            ></i>
+          <i className={`expand__toggle-icon fa ${toggleIcon}`}></i>
         </button>
       </div>
       {isOpen && <div className="expand__content">{children}</div>}
