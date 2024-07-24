cb53eb59dd4fc02b570c2514b32ea56ff08120b1
ДМИТРО <sssddd1366628@gmail.com>
2024-07-24 14:18:12 +0300

    +2

--- les11(component-updates)/task3(expanded-component-HW)/src/Expand.jsx
+++ les11(component-updates)/task3(expanded-component-HW)/src/Expand.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Expand = ({ isOpen, children, title, handleToggle }) => {
-
   return (
     <div className="app">
       <div className="expand border">
         <div className="expand__header">
           <span className="expand__title">{title}</span>
           <button className="expand__toggle-btn" onClick={handleToggle}>
-            <i
-              className={`expand__toggle-icon fa ${
-                isOpen ? "fa-chevron-up" : "fa-chevron-down"
-              }`}
-            ></i>
+            <i className={`fas ${isOpen ? "fa-chevron-up" : "fa-chevron-down"}`}></i>
           </button>
         </div>
         {isOpen && <div className="expand__content">{children}</div>}
