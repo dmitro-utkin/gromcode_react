c2d13e7badb5e9837bc849ddca7aeb4eb4fd8b58
ДМИТРО <sssddd1366628@gmail.com>
2024-07-24 12:41:13 +0300

    try todo les11/task2

--- les11(component-updates)/task2(children)/src/App.jsx
+++ les11(component-updates)/task2(children)/src/App.jsx
@@ -1,43 +1,78 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Dialog from "./Dialog.jsx";
 
-class App extends Component {
-  state = {
-    isOpen: false,
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const hideDialog = () => {
+    setIsOpen(false);
   };
 
-  hideDialog = () => {
-    this.setState({
-      isOpen: false,
-    });
+  const showDialog = () => {
+    setIsOpen(true);
   };
 
-  showDialog = () => {
-    this.setState({
-      isOpen: true,
-    });
-  }
-
-  onClick = () => {
-    this.hideDialog();
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <button className="btn" onClick={this.showDialog}>
-          Show dialog
-        </button>
-        <Dialog 
-          isOpen={this.state.isOpen} 
-          onClose={this.hideDialog} 
-          title="Recommendation"
-        >
-          <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
-        </Dialog>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <button className="btn" onClick={showDialog}>
+        Show dialog
+      </button>
+      <Dialog isOpen={isOpen} onClose={hideDialog} title="Recommendation">
+        <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
+      </Dialog>
+    </div>
+  );
+};
 
 export default App;
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import Dialog from "./Dialog.jsx";
+
+// class App = () => {
+//   state = {
+//     isOpen: false,
+//   };
+
+//   hideDialog = () => {
+//     this.setState({
+//       isOpen: false,
+//     });
+//   };
+
+//   showDialog = () => {
+//     this.setState({
+//       isOpen: true,
+//     });
+//   }
+
+//   onClick = () => {
+//     this.hideDialog();
+//   }
+
+//   render() {
+//     return (
+//       <div className="app">
+//         <button className="btn" onClick={this.showDialog}>
+//           Show dialog
+//         </button>
+//         <Dialog 
+//           isOpen={this.state.isOpen} 
+//           onClose={this.hideDialog} 
+//           title="Recommendation"
+//         >
+//           <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
+//         </Dialog>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- les11(component-updates)/task2(children)/src/Dialog.jsx
+++ les11(component-updates)/task2(children)/src/Dialog.jsx
@@ -5,15 +5,15 @@ const Dialog = ({ isOpen, children, title, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div class="dialog">
-      <div class="dialog__heading">
-        <h4 class="dialog__title">{title}</h4>
-        <button class="dialog__close-btn" onClick={onClose}>
+    <div className="dialog">
+      <div className="dialog__heading">
+        <h4 className="dialog__title">{title}</h4>
+        <button className="dialog__close-btn" onClick={onClose}>
           +
         </button>
       </div>
-      <div class="dialog__content">
-        <p>{children}</p>
+      <div className="dialog__content">
+        {children}
       </div>
     </div>
   );
