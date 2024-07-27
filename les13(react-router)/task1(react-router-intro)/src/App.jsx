798a5747d1eacb540d1046ddef59539bbee4d07f
ДМИТРО <sssddd1366628@gmail.com>
2024-07-27 16:51:53 +0300

    changed version react-router-dom to oldest

--- les13(react-router)/task1(react-router-intro)/src/App.jsx
+++ les13(react-router)/task1(react-router-intro)/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Contacts from "./Contacts.jsx";
 import Products from "./Products.jsx";
 import Home from "./Home.jsx";
@@ -9,12 +9,20 @@ const App = () => {
   return (
     <div className="page">
       <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
