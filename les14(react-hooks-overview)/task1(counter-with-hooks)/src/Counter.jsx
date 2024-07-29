abdf0f954ab51eeae05f35691c095d153c824af4
ДМИТРО <sssddd1366628@gmail.com>
2024-07-29 19:52:07 +0300

    try to fix #2

--- les14(react-hooks-overview)/task1(counter-with-hooks)/src/Counter.jsx
+++ les14(react-hooks-overview)/task1(counter-with-hooks)/src/Counter.jsx
@@ -7,16 +7,16 @@ const Counter = () => {
     <div className="counter">
       <button
         className="counter__button"
-        onClick={() => setCounter(() => counter - 1)}
+        onClick={() => setCounter(counter - 1)}
       >
         -
       </button>
-      <span className="counter__value" onClick={() => setCounter(() => 0)}>
+      <span className="counter__value" onClick={() => setCounter(0)}>
         {counter}
       </span>
       <button
         className="counter__button"
-        onClick={() => setCounter(() => counter + 1)}
+        onClick={() => setCounter(counter + 1)}
       >
         +
       </button>
