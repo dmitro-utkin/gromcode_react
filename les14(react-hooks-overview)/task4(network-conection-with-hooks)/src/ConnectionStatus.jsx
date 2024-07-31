9bf9115658baff4ec629639fb3db3bce3d73752b
ДМИТРО <sssddd1366628@gmail.com>
2024-07-31 18:40:36 +0300

    dellete comments

--- les14(react-hooks-overview)/task4(network-conection-with-hooks)/src/ConnectionStatus.jsx
+++ les14(react-hooks-overview)/task4(network-conection-with-hooks)/src/ConnectionStatus.jsx
@@ -12,86 +12,22 @@ const ConnectionStatus = () => {
       setIsOnline(false);
     };
 
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
 
     return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
     };
   }, []);
 
-  const statusClass = isOnline ? '' : 'status_offline';
+  const statusClass = isOnline ? "" : "status_offline";
 
   return (
     <div className={`status ${statusClass}`}>
-      {isOnline ? 'online' : 'offline'}
+      {isOnline ? "online" : "offline"}
     </div>
   );
 };
 
 export default ConnectionStatus;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from "react";
-
-// class ConnectionStatus extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isOnline: true
-//     };
-//   }
-
-//   handleOnline = () => {
-//     this.setState({ isOnline: true });
-//   };
-
-//   handleOffline = () => {
-//     this.setState({ isOnline: false });
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener('online', this.handleOnline);
-//     window.addEventListener('offline', this.handleOffline);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('online', this.handleOnline);
-//     window.removeEventListener('offline', this.handleOffline);
-//   }
-
-//   render() {
-//     const { isOnline } = this.state;
-//     const statusClass = isOnline ? '' : 'status_offline';
-
-//     return (
-//       <div className={`status ${statusClass}`}>
-//         {isOnline ? 'online' : 'offline'}
-//       </div>
-//     );
-//   }
-// }
-
-// export default ConnectionStatus;
